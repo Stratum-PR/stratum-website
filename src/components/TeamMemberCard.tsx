ebedd31944d86d01560ad82fd69b541ce377e9cd
Jovaniel4917 <jrodriguez4917@gmail.com>
2025-06-01 15:24:53 -0400

    Update TeamMemberCard.tsx

--- src/components/TeamMemberCard.tsx
+++ src/components/TeamMemberCard.tsx
@@ -30,7 +30,7 @@ const TeamMemberCard: React.FC<TeamMemberCardProps> = ({ founder }) => {
           <img
             src={founder.image}
             alt={`${founder.name} profile`}
-            className="w-40 h-40 mx-auto rounded-full object-cover"
+            className="w-40 h-40 mx-auto rounded-full object-contain p-2"
           />
         ) : (
           <div className="w-32 h-32 mx-auto bg-white/20 rounded-full flex items-center justify-center">
