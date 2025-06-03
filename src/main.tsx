bf7265e165665437576f0a194c6009f435c83eb3
Jovaniel Rodriguez <58681@icf.com>
2025-06-03 17:36:06 -0400

    TEst

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,16 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+    <SpeedInsights />
+  </React.StrictMode>,
+)
 
 // Performance optimization: Pre-warm the root
 const container = document.getElementById("root")!;
